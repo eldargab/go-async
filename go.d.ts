7f9428cee41e235c48f9ea75ef91e3b4b8f66c29
Eldar Gabdullin <eldargab@gmail.com>
2019-02-05 08:54:59 +0300

    add typescript definitions

---
+++ go.d.ts
@@ -0,0 +1,27 @@
+export = go
+
+
+declare function go(f: (...args) => any, ...args): go.Future<any>
+
+
+declare namespace go {
+    export class Future<T> implements Promise<T> {
+        done(err?: Error | null, val?: T): void
+
+        abort(): void
+
+        get(cb: (err?: Error, val?: T) => void): void
+
+        readonly ready: boolean
+        readonly error?: Error | null
+        readonly value?: T
+
+        onabort: () => void
+
+        then<TResult1 = T, TResult2 = never>(onfulfilled?: (value: T) => TResult1 | PromiseLike<TResult1>, onrejected?: (reason: any) => TResult2 | PromiseLike<TResult2>): Promise<TResult1 | TResult2>
+
+        catch<TResult = never>(onrejected?: (reason: any) => TResult | PromiseLike<TResult>): Promise<T | TResult>
+
+        [Symbol.toStringTag]: string;
+    }
+}
